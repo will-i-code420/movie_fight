d55ac3fa85ae7ca246ab35aa3151767d90ae66d6
Will Aronow <waronow123@gmail.com>
2021-12-05 16:21:23 -0700

    build dropdown and made fetch indiviudal movie info

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
+const dropdownContainer = document.querySelector('.dropdown-container');
+dropdownContainer.innerHTML = `
+<label for="movieInput1"><b>Movie Title</b></label>
+<input id="movieInput1" class="input" >
+<div class="dropdown">
+    <div class="dropdown-menu">
+        <div class="dropdown-content results">
+        </div>
+    </div>
+</div>
+`;
 const movieInput1 = document.querySelector('#movieInput1');
+const dropdown = document.querySelector('.dropdown');
+const results = document.querySelector('.results');
 
-const onInput = debounceInput((e) => {
-	fetchMovies(e.target.value);
+const onInput = debounceInput(async (e) => {
+	const movies = await fetchMovies(e.target.value);
+	console.log(movies);
+	dropdown.classList.add('is-active');
+	for (let movie of movies) {
+		const div = document.createElement('div');
+		div.id = movie.imdbID;
+		div.classList.add('dropdown-item');
+		div.addEventListener('click', (e) => {
+			if (!e.target.id) return fetchMovieInfo(e.target.parentElement.id);
+			fetchMovieInfo(e.target.id);
+		});
+		div.innerHTML = `
+        <img src="${movie.Poster}" />
+        <h1>${movie.Title}</h1>
+        <p class="movie-year">${movie.Year}</p>
+        `;
+		results.append(div);
+	}
 }, 500);
 
 movieInput1.addEventListener('input', onInput);
@@ -16,7 +46,8 @@ const fetchMovies = async (movie) => {
 				r: 'json'
 			}
 		});
-		return res.data;
+		if (res.data.Error) throw new Error(res.data.Error);
+		return res.data.Search;
 	} catch (e) {
 		console.log(e.message);
 	}
@@ -30,7 +61,8 @@ const fetchMovieInfo = async (id) => {
 				i: id
 			}
 		});
-		return movie.data;
+		console.log(movie.data);
+		if (movie.data.Error) throw new Error(movie.data.Error);
 	} catch (e) {
 		console.log(e.message);
 	}
