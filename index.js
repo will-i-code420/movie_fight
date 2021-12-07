1d61f0d4cd466a5219e5d6bc2d416f9a5b47175e
Will Aronow <waronow123@gmail.com>
2021-12-07 12:16:40 -0700

    added compare movies for styling

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ const autoCompleteConfig = {
 	}
 };
 
-let leftMovie;
-let rightMovie;
+let leftMovie = false;
+let rightMovie = false;
 const fetchMovieInfo = async (id, el, side) => {
 	try {
 		const movie = await axios.get('http://www.omdbapi.com/', {
@@ -41,21 +41,31 @@ const fetchMovieInfo = async (id, el, side) => {
 		if (movie.data.Error) throw new Error(movie.data.Error);
 		switch (side) {
 			case 'left':
-				leftMovie = movie.data;
+				leftMovie = true;
 				break;
 			case 'right':
-				rightMovie = movie.data;
+				rightMovie = true;
 				break;
 			default:
 				break;
 		}
 		el.innerHTML = movieInfoTemplate(movie.data);
+		if (leftMovie && rightMovie) return compareMovies();
 	} catch (e) {
 		console.log(e.message);
 	}
 };
 
 const movieInfoTemplate = (movieInfo) => {
+	const boxoffice = parseInt(movieInfo.BoxOffice.replace(/[\$\,]/g, ''));
+	const metascore = parseInt(movieInfo.Metascore);
+	const imdbRating = parseFloat(movieInfo.imdbRating);
+	const imdbVote = parseInt(movieInfo.imdbVotes.replace(/,/g, ''));
+	const awards = movieInfo.Awards.match(/(won \d+)|(\d+ wins)|/gim).join(' ').split(' ').reduce((total, val) => {
+		const value = parseInt(val);
+		if (isNaN(value)) return total;
+		return parseInt(total) + parseInt(val);
+	}, 0);
 	return `
     <article class="media">
     <figure class="media-left">
@@ -71,47 +81,65 @@ const movieInfoTemplate = (movieInfo) => {
     </div>
     </div>
     </article>
-    <article class="notification is-primary">
+    <article class="notification is-primary" data-value=${awards}>
     <p class="title">
     ${movieInfo.Awards}
     </p>
-    <p class="subtitle>
-    award
+    <p class="subtitle">
+    Awards
     </p>
     </article>
-    <article class="notification is-primary">
+    <article class="notification is-primary" data-value=${boxoffice}>
     <p class="title">
     ${movieInfo.BoxOffice}
     </p>
-    <p class="subtitle>
-    box office
+    <p class="subtitle">
+    Box Office Gross
     </p>
     </article>
-    <article class="notification is-primary">
+    <article class="notification is-primary" data-value=${metascore}>
     <p class="title">
     ${movieInfo.Metascore}
     </p>
-    <p class="subtitle>
-    metascore
+    <p class="subtitle">
+    Metascore
     </p>
     </article>
-    <article class="notification is-primary">
+    <article class="notification is-primary" data-value=${imdbRating}>
     <p class="title">
     ${movieInfo.imdbRating}
     </p>
-    <p class="subtitle>
-    imdb rating
+    <p class="subtitle">
+    IMDB Rating
     </p>
     </article>
-    <article class="notification is-primary">
+    <article class="notification is-primary" data-value=${imdbVote}>
     <p class="title">
     ${movieInfo.imdbVotes}
     </p>
-    <p class="subtitle>
-    imdb votes
+    <p class="subtitle">
+    IMDB Votes
     </p>
     </article>
     `;
+};
+
+const compareMovies = () => {
+	const leftStats = document.querySelectorAll('#left-summary .notification');
+	const rightStats = document.querySelectorAll('#right-summary .notification');
+	leftStats.forEach((leftStat, i) => {
+		const currentRight = rightStats[i];
+		const leftStatVal = parseInt(leftStat.dataset.value);
+		const rightStatVal = parseInt(currentRight.dataset.value);
+		console.log(leftStatVal, rightStatVal);
+		if (rightStatVal > leftStatVal) {
+			leftStat.classList.remove('is-primary');
+			leftStat.classList.add('is-warning');
+		} else {
+			currentRight.classList.remove('is-primary');
+			currentRight.classList.add('is-warning');
+		}
+	});
 };
 
 createAutoComplete({
@@ -119,7 +147,7 @@ createAutoComplete({
 	rootEl: document.querySelector('#left-autocomplete'),
 	onItemSelect(itemId) {
 		document.querySelector('.tutorial').classList.add('is-hidden');
-		fetchMovieInfo(itemId, document.querySelector('#left-summary', 'left'));
+		fetchMovieInfo(itemId, document.querySelector('#left-summary'), 'left');
 	}
 });
 createAutoComplete({
@@ -127,6 +155,6 @@ createAutoComplete({
 	rootEl: document.querySelector('#right-autocomplete'),
 	onItemSelect(itemId) {
 		document.querySelector('.tutorial').classList.add('is-hidden');
-		fetchMovieInfo(itemId, document.querySelector('#right-summary', 'right'));
+		fetchMovieInfo(itemId, document.querySelector('#right-summary'), 'right');
 	}
 });
